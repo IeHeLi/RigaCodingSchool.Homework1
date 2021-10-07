a1c06ab7f15d805ef8bd2cbc34581c7c9c97917a
ieval <ieval@LAPTOP-SQ4F164V>
2021-10-07 20:45:36 +0300

    Add project files.

---
+++ RigaCodingSchool.Homework1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RigaCodingSchool.Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello there!!");
+            Console.WriteLine("My name is Ieva and I would like to introduce myself!");
+            Console.WriteLine("I am a begginer to coding, and this is my first time trying anything!");
+            Console.WriteLine("I am still a student and I study Sociology!");
+            Console.WriteLine("In my free time I love cooking and reading books!");
+        
+            Console.ReadLine();
+        }
+    }
+}
