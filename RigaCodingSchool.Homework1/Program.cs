f9ddd2d79e4f7c1fd4ae435135af0b64d7d05b1f
ievali <ieval@LAPTOP-SQ4F164V>
2021-10-13 16:15:19 +0300

    New Update HW

--- RigaCodingSchool.Homework1/Program.cs
+++ RigaCodingSchool.Homework1/Program.cs
@@ -6,14 +6,30 @@ namespace RigaCodingSchool.Homework2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello there!!");
-            Console.WriteLine("My name is Ieva and I would like to introduce myself!");
-            Console.WriteLine("I am a begginer to coding, and this is my first time trying anything!");
-            Console.WriteLine("I am still a student and I study Sociology!");
-            Console.WriteLine("In my free time I love cooking and reading books!");
-            //comment
-        
+            #region variables
+            byte age = 24;
+            string name = "Ieva Helēna";
+            string surname = "Litinska";
+            double height = 1.61;
+            string size = "short";
+            string hello = "Hi students of our course!";
+            string hobby = "cooking, reading and relaxing";
+            byte year = 3;
+            string degree = "sociology";
+            //This is the part with the variables
+            #endregion
+
+            #region theCode
+            Console.WriteLine(hello);
+            Console.WriteLine($"I am  {name} { surname} .");
+            Console.WriteLine($"My age is {age}. And my height is {height}m, so I am quite {size}.");
+            Console.WriteLine($"In my free time I enjoy {hobby}.");
+            Console.WriteLine($"I currently study {degree} and I am in my {year}rd year.");
+            Console.WriteLine("Hope you have a Wonderful day Today! :) ");
             Console.ReadLine();
+            //This is the part, which will show
+            #endregion 
+
         }
     }
 }
